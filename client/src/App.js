5a5b65360270de49d63319b4200ca0b74259dffd
Marcelo <marcelo.trajano@outlook.com>
2020-10-17 20:34:38 -0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+import "./index.css";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { getLocations } from "./api/Api";
 import Location from "./components/Location";
+import MapPin from "./assets/map-pin.svg";
+const MAPBOX_API_ACCESS_TOKEN =
+  "pk.eyJ1IjoibWFyY2Vsb3RyYWphbm8iLCJhIjoiY2tmem05cHpwMXlsbTJ6cDlnaWY5dzR2bSJ9.7F7O_Onlh5gVxa8vz2FIuA";
 
 function App() {
   const [locations, setLocations] = useState([]);
+
+  const [showPopup, setShowPopup] = useState(true);
 
   const [viewport, setViewport] = useState({
     width: "100vw",
@@ -22,11 +28,10 @@ function App() {
     })();
   }, []);
 
-  console.log(process.env.MAPBOX_API_ACCESS_TOKEN);
-
   return (
     <ReactMapGL
-      mapboxApiAccessToken={process.env.MAPBOX_API_ACCESS_TOKEN}
+      mapStyle="mapbox://styles/marcelotrajano/ckfzol8hj1h3y19nznrlp55z3"
+      mapboxApiAccessToken={MAPBOX_API_ACCESS_TOKEN}
       {...viewport}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
     >
@@ -38,7 +43,19 @@ function App() {
           offsetLeft={-20}
           offsetTop={-10}
         >
-          <Location location={location} />
+          <svg
+            viewBox="0 0 24 24"
+            width="35"
+            height="35"
+            stroke="#F96150"
+            strokeWidth="2"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+            <circle cx="12" cy="10" r="3"></circle>
+          </svg>
         </Marker>
       ))}
     </ReactMapGL>
